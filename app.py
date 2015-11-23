390b7807189df029b2c19ffd2607c49579bc3f2c
Matt <welshmattd@gmail.com>
2015-11-23 13:36:11 -0600

    Added functional main loop in bot.py with testing functions, cleaned up import headers and comments

--- app.py
+++ app.py
@@ -4,40 +4,39 @@ import network
 
 # Receive data from socket, decode it, print to console
 def receive(sock):
-	# Receive data from socket
-	msg = sock.recv(4096)
-	# Turn into a string so we can parse it
-	msg = network.decode(msg)
-	# Print to console
-	print(msg.strip("\r\n"))
+	msg = sock.recv(4096)		# Receive data from socket
+	msg = network.decode(msg)	# Turn into a string so we can parse it
+	print(msg.strip("\r\n"))	# Print to console
 	return msg
 
 # Print data to transmit to console, encode it, send to socket
-def transmit(sock, rsp):
-	print("... " + rsp.strip("\r\n"))
-	# Encode our response
-	rsp = network.encode(rsp)
-	sock.send(rsp)
+def transmit(sock, msg):
+	print("... " + msg.strip("\r\n"))	# Print to console
+	msg = network.encode(msg)			# Encode our message
+	sock.send(msg)						# Send our message
 
 # IRC handshake used to negotiate with server and join channel
 def irc_handshake(sock, nick, chan):
+	# Stage 0: define what we're looking for in the transactions
+	ident_trigger = "Checking Ident"
+	join_trigger = "MODE"
 	# Stage 1: send our ident once the server requests it
 	while True:
 		msg = receive(sock)
-		if "Checking Ident" in msg:
-			# Send USER command
-			user_cmd = irc.user()
-			transmit(sock, user_cmd)
-			# Send NICK command
-			nick_cmd = irc.nick(nick)
-			transmit(sock, nick_cmd)
+		if ident_trigger in msg:
+			user_cmd = irc.user()		# Formulate USER command
+			transmit(sock, user_cmd)	# Send USER command
+			nick_cmd = irc.nick(nick)	# Formulate NICK command
+			transmit(sock, nick_cmd)	# Send NICK command
 			break
 	# Stage 2: join channel once the server sets our mode
 	while True:
 		msg = receive(sock)
 		# Check if the server has set a mode for us
-		if "MODE" in msg:
-			# Send JOIN command
-			join_cmd = irc.join(chan)
-			transmit(sock, join_cmd)
+		if join_trigger in msg:
+			join_cmd = irc.join(chan)	# Formulate JOIN command
+			transmit(sock, join_cmd)	# Send JOIN command
 			break
+
+def quit():
+	pass
--- bot.py
+++ bot.py
@@ -1,16 +1,16 @@
 
 import app
-import sys
 import network
+import socket
+import sys
+import time
 
 server = sys.argv[1]	# e.g. irc.derpnet.net
 channel = "#" + sys.argv[2]	# e.g. derpnet
 nickname = sys.argv[3]	# e.g. derpbot
 
 auth_users = []
-# response_dict = {
-	# "!time":
-# }
+trigger_dict = {}
 
 # Create socket which will connect to the server
 connection = network.create_sock(server)
@@ -18,6 +18,15 @@ connection = network.create_sock(server)
 # Initiate the IRC handshake sequence
 # At the end of this, we will be in the specified channel
 app.irc_handshake(connection, nickname, channel)
+# Make this socket non-blocking with a 1.0s timeout
+connection.settimeout(1.0)
 
 while True:
-	msg = app.receive(connection)
+	try:
+		msg = app.receive(connection)
+	except KeyboardInterrupt:
+		connection.shutdown(socket.SHUT_RDWR)
+		connection.close()
+		exit("ctrl+C Interrupt detected")
+	except socket.timeout:
+		print("kek")
